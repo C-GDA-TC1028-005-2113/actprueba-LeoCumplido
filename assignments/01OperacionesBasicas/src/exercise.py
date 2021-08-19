58f90c2676ba9446c5b46441aca25ed9965c111f
LeoCumplido <a01026491@itesm.mx>
2021-08-19 15:30:17 +0000

    Envio_1

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    num1 = int(input("Dame un número: "))
+    num2 = int(input("Dame un número: "))
+    suma = num1 + num2
+    resta = num1 - num2
+    mult = num1 * num2
+    
+    print(f"Suma: {suma}")
+    print(f"Resta: {resta}")
+    print(f"Multiplicación: {mult}")
 
 
 
